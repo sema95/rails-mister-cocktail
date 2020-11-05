a9d3ece2170821accaac4907a1e0b5c9314011cc
Max Sevenig <max.sevenig@hotmail.com>
2020-11-05 16:56:58 +0000

    Mister cocktail done without css etc...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'rest-client'
+require 'json'
+
+ingredient_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+list = RestClient.get(ingredient_url)
+result = JSON.parse(list)
+
+puts 'Destroying all Ingredients'
+Ingredient.destroy_all
+puts 'Creating fake ingredients'
+
+result['drinks'].each do |ingredient|
+  Ingredient.create!(name: ingredient['strIngredient1'])
+end
+puts 'Created fake ingredients'
